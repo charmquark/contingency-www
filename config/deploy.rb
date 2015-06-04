6a1cffe5cff33712d078b468967fdd87bd119895
csauls <ibisbasenji@gmail.com>
2015-06-04 03:45:24 -0500

    deployment fun

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,13 +52,6 @@ namespace :deploy do
         end
     end
     
-    after 'git:create_release', 'app:dotenv' do
-        on roles(:app) do |host|
-            env_file = ".env.#{fetch :rails_env}"
-            execute "cp #{deploy_path}/#{env_file} #{release_path}/#{env_file}"
-        end
-    end
-    
     after :published, 'app:start' do
         on roles(:app) do |host|
             within release_path do
@@ -77,5 +70,16 @@ namespace :deploy do
 #             # end
 #         end
 #     end
+
+end
+
+namespace :git do
+
+    after :create_release, 'app:dotenv' do
+        on roles(:app) do |host|
+            env_file = ".env.#{fetch :rails_env}"
+            execute "cp #{deploy_path}/#{env_file} #{release_path}/#{env_file}"
+        end
+    end
 
 end
