5dde39296dd4772b0fc35ab4d0759170b2426a9b
csauls <ibisbasenji@gmail.com>
2015-06-28 17:02:04 -0500

    rip

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -18,19 +18,6 @@ module GamesHelper
         image_tag game.banner.url, options
     end
     
-#     def game_banner(game, img_options = {}, link_options = {})
-#         class_opt = "game-banner #{game.featured? ? 'game-featured' : ''} "
-#         
-#         img_options = img_options.symbolize_keys
-#         img_options[:alt] = img_options.fetch(:alt, game.name)
-#         img_options[:class] = class_opt + img_options.fetch(:class, '')
-#         
-#         link_options = link_options.symbolize_keys
-#         link_options[:class] = class_opt + link_options.fetch(:class, '')
-# 
-#         link_to(image_tag(game.banner.url, img_options), game, link_options)
-#     end
-    
     def games_banners(games, img_options = {}, link_options = {})
         games_banners_list(games, img_options, link_options).join('').html_safe
     end
