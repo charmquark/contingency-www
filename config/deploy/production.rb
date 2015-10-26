13f18dfacb3d519dfd7faf0ec79928da6c90f91d
csauls <ibisbasenji@gmail.com>
2015-10-26 00:58:34 -0500

    updating deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'thecontingency.org', user: 'git-deploy', roles: %w(app db web)
+server 'thecontingency.org', user: 'git-www', roles: %w(app db web)
 
 
 # role-based syntax
